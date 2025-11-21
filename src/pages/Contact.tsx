94bce2dec44b053ec128abb5012da31005033e78
arkglobal <arkglobal@example.com>
2025-11-21 15:46:42 +0530

    Fix Google Maps iframe in Contact page

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -261,7 +261,7 @@ const Contact: React.FC = () => {
           </div>
           <div className="map-container-full">
             <iframe 
-              src="https://maps.app.goo.gl/Gkrxjp4Zjz7zrsti9"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3721.0123456789!2d79.0881979!3d21.1458004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bd4c0a5a31faf13%3A0x19233d3b9146d4c!2sNandanvan%2C%20Nagpur%2C%20Maharashtra%20440024!5e0!3m2!1sen!2sin!4v1234567890123!5m2!1sen!2sin"
               width="100%" 
               height="400" 
               style={{border: 0}} 
